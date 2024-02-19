3a0567aa271d138de0c53d272dc1e141a540f921
Reen00ad <105815655+Reen00ad@users.noreply.github.com>
2024-02-19 10:48:32 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        //ex1
+//
+//        int large=0;
+//        System.out.println("Please enter first number : ");
+//        int num1 = s.nextInt();
+//        System.out.println("Please enter seconde number : ");
+//        int num2 = s.nextInt();
+//        System.out.println("Please enter third number : ");
+//        int num3 = s.nextInt();
+//
+//        if(num1 > num2 && num1 > num3)
+//            large=num1;
+//        else if (num2 > num1 && num2 > num3)
+//            large=num2;
+//        else
+//            large=num3;
+//        System.out.println("the largest number is "+large);
+
+        //ex2
+//
+//        System.out.println("enter a string : ");
+//        String word=s.nextLine();
+//        System.out.println("enter a number : ");
+//        int num=s.nextInt();
+//        System.out.println(word.charAt(num));
+
+        //ex3
+//        int sum=0;
+//        System.out.println("enter number (or enter 0 to quiet ): ");
+//        int num = s.nextInt();
+//        while(num != 0){
+//            sum=sum+num;
+//            num = s.nextInt();
+//            }
+//
+//
+//        System.out.println("the sum of number is : "+sum);
+
+        //ex4
+//        int [] num = {10,-21,30,31,-25};
+//        for (int i = 0; i < num.length; i++) {
+//            if(num[i]>0)
+//                System.out.println(num[i]+" is a positive number ");
+//            else
+//                System.out.println(num[i]+" is a negative number ");
+//
+//        }
+
+        //ex5
+
+//        String sh = "";
+//
+//         String [] word={"tuwaiq","bootcamp","student","java"};
+//        for (int i = 0; i < word.length; i++) {
+//          if (word[0].length()<sh.length())
+//            sh = word[0];
+//           else if(word[1].length()<sh.length())
+//                sh=word[1];
+//            else if (word[2].length()<sh.length())
+//                sh=word[2];
+//            else
+//                sh=word[3];
+//
+//        }System.out.println(sh);
+
+
+
+    }
+}
